a74f04eb0e01f2bbc12bf453a1cc95cd065f0e7d
Cassie Musolf <cassiemusolf@gmail.com>
2017-04-07 10:10:03 -0700

    add service and inject it into volunteer component, list volunteer names from firebase.

--- src/app/volunteers/volunteers.component.ts
+++ src/app/volunteers/volunteers.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { Volunteer } from '../volunteer.model';
+import { Router } from '@angular/router';
+import { AngularFire, FirebaseListObservable } from 'angularfire2';
+import { VolunteerService } from '../volunteer.service';
 
 @Component({
   selector: 'app-volunteers',
   templateUrl: './volunteers.component.html',
-  styleUrls: ['./volunteers.component.css']
+  styleUrls: ['./volunteers.component.css'],
+  providers: [VolunteerService]
 })
 export class VolunteersComponent implements OnInit {
+  volunteers: FirebaseListObservable<any[]>;
 
-  constructor() { }
+  constructor(private router: Router, private volunteerService: VolunteerService) { }
 
   ngOnInit() {
+    this.volunteers = this.volunteerService.getVolunteers();
   }
 
 }
